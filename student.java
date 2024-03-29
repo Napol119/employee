925856ae497b2d407b49df78c85ab3f6e6a7e3ae
Napol119 <152479370+Napol119@users.noreply.github.com>
2024-03-29 14:28:44 +0700

    Create student.java

---
+++ student.java
@@ -0,0 +1,14 @@
+package student;
+
+public class Student {
+	 private String course;
+	 protected float score;
+
+	 public String getCourse() {
+	 return course;
+	 }
+	 public void setScore(String stdCourse, float stdScore) {
+	 course = stdCourse;
+	 score = stdScore;
+	 }
+	}
